971c15deabceaab1f13f9c341cb0bdb0cafd3819
Zachary Chavez <zpchavez@gmail.com>
2017-07-31 21:42:03 -0700

    Remove commented out code

--- server.js
+++ server.js
@@ -10,14 +10,6 @@ app.get('*', function(req, res) {
   }
 });
 
-// app.get('/', function(req, res) {
-//   res.sendFile('index.html', { root: './' });
-// });
-//
-// app.get('/app.js', function(req, res) {
-//   res.sendFile('app.js', { root: './' });
-// });
-
 app.listen(PORT, function (err, result) {
   if (err) {
     console.log(err);
