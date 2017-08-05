a74c5c0686140cb27f982abc6772bf2eeaa883dc
Zachary Chavez <zpchavez@gmail.com>
2017-08-05 16:17:42 -0700

    basic ga setup

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-import Ga from '../lib/ga';
+require('../lib/ga');
+const ga = window.ga;
+
+const play = () => {
+
+}
+
+var g = ga(
+  512,
+  512,
+  () => {
+    g.canvas.style.border = "1px black dashed";
+    g.canvas.style.display = "block";
+    g.canvas.style.margin = "auto";
+    g.backgroundColor = "white";
+    g.state = play;
+  }
+);
+
+g.start();
